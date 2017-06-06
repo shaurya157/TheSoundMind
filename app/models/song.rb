1919069c11e3d8df8564a72be72c94c93b450386
Shaurya Sinha <shaurya157@gmail.com>
2017-06-06 21:59:42 +0700

    Song model done

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,24 @@
 class Song < ApplicationRecord
+  validates :url, presence: true, uniqueness: true
+  validates :artist, :name, :duration, presence: true
+
+  after_initialize :first_impression
+
+  def first_impression
+    self.impressions_in_recommendations = 0
+  end
+
+  def set_duration(duration)
+    # TODO: Handles songs lesser than one hour, need to fix later on
+    
+    if duration.is_a?(Integer)
+      self.duration = duration
+    else
+      arr = duration.split(":")
+      arr[0] = arr[0].to_i * 60
+      arr[1] = arr[1].to_i
+
+      self.duration = arr.inject(:+)
+    end
+  end
 end
